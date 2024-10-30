ce32b2766f494f93cd1c2e7bcb82d78b615d1c89
Plastimytes <magezirichardelijah@gmail.com>
2024-10-30 09:04:17 +0300

    Update exp.py

--- exp.py
+++ exp.py
@@ -1,5 +1,10 @@
+## User input
 num=int(input("Enter a number "))
+
+#initialize the while loop
 number=0
+
+##While loop to find out if the number is even
 while num%2  != 0:
     print(f"{num} is an odd number")
     num=int(input("Enter a number "))
